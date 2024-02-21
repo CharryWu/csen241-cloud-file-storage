2268bc302227b00b2aa96d5fcb7261335ae71018
PeterZhu <74828967+soy-crypto@users.noreply.github.com>
2024-02-21 23:57:59 +0000

    02-21

--- main.py
+++ main.py
@@ -8,6 +8,17 @@ from fastapi import FastAPI, UploadFile, File, HTTPException
 from starlette.responses import StreamingResponse
 from fastapi import FastAPI, UploadFile, Response, File
 from starlette.responses import StreamingResponse
+from fastapi import FastAPI, Depends, templating
+from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
 # define an app
 app = FastAPI()
@@ -19,20 +30,20 @@ templates = Jinja2Templates(directory="templates")
 # define a homepage
 @app.get("/")
 async def homepage(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="index.html")
 
 
-@app.get("/upload")
+@app.get("/upload", response_class=HTMLResponse)
 async def upload(request: Request):
-    return templates.TemplateResponse("upload.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="upload.html")
 
 
-@app.get("/download")
+@app.get("/download", response_class=HTMLResponse)
 async def download(request: Request):
-    return templates.TemplateResponse("download.html", {"request": request})
+    return templates.TemplateResponse(request=request, name="download.html")
+
 
 # Data structure to store uploaded files (replace with database or desired method)
-
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
   # Validate and save file
